875d63676b40ed48245065573980e56250eb438d
Ivan8or <ivanthegreat825@gmail.com>
2025-09-27 09:53:47 -0400

    no period oopsss

--- app-backend/src/index.js
+++ app-backend/src/index.js
@@ -54,7 +54,7 @@ http.createServer(async (req, res) => {
   }
 
   console.log(`url '${path}', extension '${extension}'`);
-  if(splitpath.length > 1 && /\.(png)|(jpg)|(jpeg)|(ico)|(gif)|(tiff)|(svg)|(webp)|(avif)|(img)$/.test(extension)) {
+  if(splitpath.length > 1 && /(png)|(jpg)|(jpeg)|(ico)|(gif)|(tiff)|(svg)|(webp)|(avif)|(img)$/.test(extension)) {
     console.log("this is an image (2), handling it as such...");
     const image_name = splitpath.slice(0, -1).join("-").split("/").at(-1);
     const image_remote_path = `${image_server}/${image_name}`;
