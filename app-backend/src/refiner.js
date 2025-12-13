5bbc49c4e961b21cfed77b252905f0859e440c9e
Ivan8or <ivanthegreat825@gmail.com>
2025-12-12 22:36:41 -0500

    null safety for refiner

--- app-backend/src/refiner.js
+++ app-backend/src/refiner.js
@@ -4,7 +4,7 @@ export function refineResponse(req, response) {
   // get rid of foreign domains
   return he.decode(
     response
-      .replaceAll(/http(s)?:\/\/[a-zA-Z0-9-.]+/g, "")
-      .replace("<head>", `<head><link rel=\"icon\" type=\"image/x-icon\" href=\"${req.url}.ico\">`)
+      ?.replaceAll(/http(s)?:\/\/[a-zA-Z0-9-.]+/g, "")
+      ?.replace("<head>", `<head><link rel=\"icon\" type=\"image/x-icon\" href=\"${req.url}.ico\">`)
   );
 }
